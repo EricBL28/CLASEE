3e9f51673e0f1e54435ed168d96750aac3965a24
EricBL28 <2061784@alu.murciaeduca.es>
2024-12-02 17:24:13 +0100

    asdasd

--- EntregablePSP/Ejercicio1/clienteTCP/src/main/java/org/example/Main.java
+++ EntregablePSP/Ejercicio1/clienteTCP/src/main/java/org/example/Main.java
@@ -1,10 +1,23 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("asdasdasd");
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.net.Socket;
 
+public class Main {
+    static final String HOST = "localhost";
+    static final int Puerto = 2000;
+
+
+    public static void main(String[] args) {
+        try {
+            Socket sCliente = new Socket(HOST, Puerto);
+            InputStream aux = sCliente.getInputStream();
+            DataInputStream flujo_entrada = new DataInputStream(aux);
+            System.out.println(flujo_entrada.readUTF());
+            sCliente.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
