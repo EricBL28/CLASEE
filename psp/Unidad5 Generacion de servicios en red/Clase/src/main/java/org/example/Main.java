7ce7268c2e08495af79179894b144159f57c77fa
DAM <2061784@alu.murciaeduca.es>
2025-02-03 17:28:50 +0100

    FTP prueba

--- psp/Unidad5 Generacion de servicios en red/Clase/src/main/java/org/example/Main.java
+++ psp/Unidad5 Generacion de servicios en red/Clase/src/main/java/org/example/Main.java
@@ -1,8 +1,44 @@
 package org.example;
 
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hola");
+
+    public static void main(String[] args) {
+
+        try{
+
+            FTPClient clienteFTP = new FTPClient();
+
+            clienteFTP.connect("ftp.rediris.es");
+
+             int respuesta=clienteFTP.getReplyCode();
+
+            if(!FTPReply.isPositiveCompletion(respuesta)){
+
+
+                //abre una sesión con el usuario anónimo
+                clienteFTP.login("alumno", "alumno");
+                //Activar el modo pasivo para no tener problemas con el Firewall de Windows
+                clienteFTP.enterLocalPassiveMode();
+                //lista las carpetas de primer nivel del servidor FTP
+                System.out.println("Carpetas disponibles en Servidor:");
+
+                clienteFTP.disconnect();
+                System.out.println("Descarga finalizada correctamente");
+            }else{
+                clienteFTP.disconnect();
+                System.err.println("FTP ha rechazado la conexión esblecida");
+                System.exit(1);
+            }
+
+
+        }catch ( IOException e ){
+            System.out.println(e.getMessage());
+        }
+
     }
 }
